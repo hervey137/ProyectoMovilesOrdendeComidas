edb1dfc4063666b48aa6cd20467e272c602d8715
hervey14 <hervey1378@gmail.com>
2024-04-28 13:42:22 -0700

    Acomodando Proyectos de Github

--- ProyectoOrdendeComidasEquipo2/app/src/main/java/mx/edu/potros/proyectoordendecomidasequipo2/RegisterActivity.kt
+++ ProyectoOrdendeComidasEquipo2/app/src/main/java/mx/edu/potros/proyectoordendecomidasequipo2/RegisterActivity.kt
@@ -13,8 +13,8 @@ class RegisterActivity : AppCompatActivity() {
         val button: Button = findViewById(R.id.btn_register)
 
         button.setOnClickListener {
-            val intentSign: Intent = Intent(this, SignActivity::class.java)
-            startActivity(intentSign)
+            var intent: Intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
         }
     }
 }
